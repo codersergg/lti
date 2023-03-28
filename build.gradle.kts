57578874573ef8faee5f12e2eb086a51418da412
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-03-28 16:31:21 +0300

    d

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val commonsCodecVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("io.ktor.plugin") version "2.2.4"
+    id("io.ktor.plugin") version "2.2.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
 }
 

2b386070782e45f57165d7d3ed094aff1bfac1c9
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-03-29 11:23:48 +0300

    Update Application.kt

--- src/main/kotlin/com/codersergg/Application.kt
+++ src/main/kotlin/com/codersergg/Application.kt
@@ -1,6 +1,7 @@
 package com.codersergg
 
 import com.codersergg.data.MemoryInitLoginDataSource
+import com.codersergg.data.MemoryTestDataSource
 import com.codersergg.plugins.*
 import io.ktor.server.application.*
 
@@ -9,7 +10,8 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused")
 fun Application.module() {
     val initLoginDataSource = MemoryInitLoginDataSource()
+    val testDataSource = MemoryTestDataSource()
 
     configureSerialization()
-    configureRouting(initLoginDataSource = initLoginDataSource)
+    configureRouting(initLoginDataSource = initLoginDataSource, testDataSource = testDataSource)
 }

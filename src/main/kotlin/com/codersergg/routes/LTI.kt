6dc9b808ccd3f2f856e3350b5e9e4d7e483dbba7
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-04-13 10:30:36 +0200

    set security

--- src/main/kotlin/com/codersergg/routes/LTI.kt
+++ src/main/kotlin/com/codersergg/routes/LTI.kt
@@ -169,6 +169,7 @@ fun Route.authenticationResponsePost(
         println("payload: $payload")
 
         val publicKey = jwkProvider.get("6f8856ed-9189-488f-9011-0ff4b6c08edc").publicKey
+        println("publicKey: $publicKey")
         // https://stackoverflow.com/questions/6559272/algid-parse-error-not-a-sequence
         val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode("MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAtfJaLrzXILUg1U3N1KV8yJr92GHn5OtYZR7qWk1Mc4cy4JGjklYup7weMjBD9f3bBVoIsiUVX6xNcYIr0Ie0AQIDAQABAkEAg+FBquToDeYcAWBe1EaLVyC45HG60zwfG1S4S3IB+y4INz1FHuZppDjBh09jptQNd+kSMlG1LkAc/3znKTPJ7QIhANpyB0OfTK44lpH4ScJmCxjZV52mIrQcmnS3QzkxWQCDAiEA1Tn7qyoh+0rOO/9vJHP8U/beo51SiQMw0880a1UaiisCIQDNwY46EbhGeiLJR1cidr+JHl86rRwPDsolmeEF5AdzRQIgK3KXL3d0WSoS//K6iOkBX3KMRzaFXNnDl0U/XyeGMuUCIHaXv+n+Brz5BDnRbWS+2vkgIe9bUNlkiArpjWvX+2we"))
         val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpecPKCS8)

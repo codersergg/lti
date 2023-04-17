d75674534d0aae8aee1f1c18b22e8eab567b8c64
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-04-18 01:26:13 +0200

    set security

--- src/main/kotlin/com/codersergg/routes/LTIv1p1.kt
+++ src/main/kotlin/com/codersergg/routes/LTIv1p1.kt
@@ -46,7 +46,7 @@ fun garde(parameters: Parameters) {
     val publicKey = parameters["oauth_consumer_key"]
     val secretKey = "privateKey"
     val lisResultSourcedid = parameters["lis_result_sourcedid"]
-    IMSPOXRequest.sendReplaceResult(urlString, publicKey, secretKey, lisResultSourcedid, "0.0")
+    IMSPOXRequest.sendReplaceResult(urlString, publicKey, secretKey, lisResultSourcedid, "0.5")
 }
 
 private fun verifyRequest(parameters: Parameters): Boolean {

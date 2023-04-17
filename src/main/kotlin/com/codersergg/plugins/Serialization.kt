ae7da76b36d6f338c6a41ca95e5be1b9788ba02e
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-04-17 17:24:39 +0200

    set security

--- src/main/kotlin/com/codersergg/plugins/Serialization.kt
+++ src/main/kotlin/com/codersergg/plugins/Serialization.kt
@@ -6,9 +6,9 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 
 fun Application.configureSerialization() {
-    install(ContentNegotiation) {
+    /*install(ContentNegotiation) {
         json()
-    }
+    }*/
     install(ContentNegotiation) {
         xml()
     }
